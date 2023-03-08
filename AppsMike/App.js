b5518895bd0059f1ee00fca7f838748b750fad94
tnmod <tnmod2003@gmail.com>
2023-03-08 15:45:58 +0700

    UPDATE v1.5

---
+++ AppsMike/App.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import Login from './src/login/Login';
+
+const App = () => {
+  return (
+    <View>
+      <Login />
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
